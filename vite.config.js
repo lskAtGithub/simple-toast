e1856dbff0b2ad1ce55ea98134988976c4eb2486
liushegnkai <949919743@qq.com>
2024-01-28 20:03:31 +0800

    build: build config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite'
 import typescript from '@rollup/plugin-typescript'
+import babel from '@rollup/plugin-babel'
 
 export default defineConfig({
-  plugins: [typescript()],
+  plugins: [
+    typescript({
+      target: 'esnext',
+      emitDeclarationOnly: true,
+    }),
+    babel({
+      extensions: ['.es6', '.es', '.mjs'],
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'MyLib',
       fileName: 'my-lib',
-      formats: ['es', 'cjs'],
+      formats: ['es', 'umd', 'cjs'],
+    },
+    rollupOptions: {
+      external: [/\.es\.js$/],
     },
   },
 })
